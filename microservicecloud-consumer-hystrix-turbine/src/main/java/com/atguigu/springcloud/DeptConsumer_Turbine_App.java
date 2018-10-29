79eabd07e964496145250107bae77f9377d55f1f
yongshen <168_shen@sina.com>
2018-10-29 13:19:48 +0800

    提交

--- microservicecloud-consumer-hystrix-turbine/src/main/java/com/atguigu/springcloud/DeptConsumer_Turbine_App.java
+++ microservicecloud-consumer-hystrix-turbine/src/main/java/com/atguigu/springcloud/DeptConsumer_Turbine_App.java
@@ -2,11 +2,9 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableTurbine
 public class DeptConsumer_Turbine_App
 {
